759c15cc6097193a0629fea43f6d82e04cdd0c06
Crabislav <alex.kopnin2@gmail.com>
2020-12-07 19:19:24 +0200

    add homework2

---
+++ src/main/java/lesson3/homework2/Demo.java
@@ -0,0 +1,17 @@
+package lesson3.homework2;
+
+public class Demo {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println("Save performance = " + solution.testSavePerformance());
+        System.out.println("Delete all by single query" + solution.testDeletePerformance());
+
+        solution.testSavePerformance();
+        System.out.println("Delete by ID " + solution.testDeleteByIdPerformance());
+
+        solution.testSavePerformance();
+        System.out.println("SELECT by single query" + solution.testSelectPerformance());
+        System.out.println("SELECT by ID" + solution.testSelectByIdPerformance());
+    }
+}
---
+++ src/main/java/lesson3/homework2/Solution.java
@@ -0,0 +1,123 @@
+package lesson3.homework2;
+
+import java.sql.*;
+
+public class Solution {
+    private static final String DB_URL = "jdbc:oracle:thin:@gromcode-lessons.cgumkbozheyb.eu-west-3.rds.amazonaws.com:1521:ORCL";
+    private static final String USER = "main";
+    private static final String PASSWORD = "gromcode";
+
+    private static final String INSERT_INTO_TEST_SPEED = "INSERT INTO TEST_SPEED VALUES (?,?,?)";
+    private static final String DELETE_FROM_TEST_SPEED_BY_ID = "DELETE FROM TEST_SPEED WHERE ID=?";
+    private static final String DELETE_ALL_FROM_TEST_SPEED_BY_ID = "DELETE FROM TEST_SPEED";
+    private static final String SELECT_FROM_TEST_SPEED_BY_SINGLE_ID = "SELECT * FROM TEST_SPEED WHERE ID=?";
+    private static final String SELECT_ALL_FROM_TEST_SPEED_BY_ID = "SELECT *  FROM TEST_SPEED";
+
+    private static final int ROWS_AMOUNT = 1000;
+
+    //Все методы должны замерять скорость операции в миллисекундах и возвращать полученное значение
+
+    //будет успешно добавлять 1000 записей в таблицу TEST_SPEED c произвольными значениями
+    //103404
+    long testSavePerformance() {
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_INTO_TEST_SPEED)) {
+
+            long start = System.currentTimeMillis();
+            for (int i = 0; i < ROWS_AMOUNT; i++) {
+                preparedStatement.setLong(1, i);
+                preparedStatement.setString(2, "string" + i);
+                preparedStatement.setInt(3, i * 10);
+
+                preparedStatement.executeUpdate();
+            }
+            long finish = System.currentTimeMillis();
+
+            return finish - start;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    //будет удалять 1000 добавленных перед этим записей, отдельными запросами по полю ID
+    //106088
+    long testDeleteByIdPerformance() {
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(DELETE_FROM_TEST_SPEED_BY_ID)) {
+
+            long start = System.currentTimeMillis();
+            for (int i = 0; i < ROWS_AMOUNT; i++) {
+                preparedStatement.setLong(1, i);
+                preparedStatement.executeUpdate();
+            }
+            long finish = System.currentTimeMillis();
+
+            return finish - start;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    //будет удалять 1000, одним SQL запросом
+    //206
+    long testDeletePerformance() {
+        try (Connection connection = getConnection();
+             Statement statement = connection.createStatement()) {
+
+            long start = System.currentTimeMillis();
+            statement.executeQuery(DELETE_ALL_FROM_TEST_SPEED_BY_ID);
+            long finish = System.currentTimeMillis();
+
+            return finish - start;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    //будет выбирать по очереди 1000 добавленных перед этим записей, отдельными запросами
+    //по полю ID
+    //103220
+    long testSelectByIdPerformance() {
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_FROM_TEST_SPEED_BY_SINGLE_ID)) {
+
+            long start = System.currentTimeMillis();
+            for (int i = 0; i < ROWS_AMOUNT; i++) {
+                preparedStatement.setLong(1, i);
+                preparedStatement.executeQuery();
+            }
+            long finish = System.currentTimeMillis();
+
+            return finish - start;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+
+    //будет выбирать 1000 записей, одним SQL запросом
+    //308
+    long testSelectPerformance() {
+        try (Connection connection = getConnection();
+             Statement statement = connection.createStatement()) {
+
+            long start = System.currentTimeMillis();
+            statement.executeQuery(SELECT_ALL_FROM_TEST_SPEED_BY_ID);
+            long finish = System.currentTimeMillis();
+
+            return finish - start;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    private Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL, USER, PASSWORD);
+    }
+}
+
