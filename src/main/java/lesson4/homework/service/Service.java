9c3415609522327235a86da5ec0cc6db828f4de5
Crabislav <alex.kopnin2@gmail.com>
2020-12-22 22:36:54 +0200

    add throws InvalidInputException to CRUD methods at Service.java

--- src/main/java/lesson4/homework/service/Service.java
+++ src/main/java/lesson4/homework/service/Service.java
@@ -2,13 +2,14 @@ package lesson4.homework.service;
 
 import lesson4.homework.exception.InvalidInputException;
 
-
 public interface Service<T> {
     T save(T t) throws InvalidInputException;
 
-    void delete(long id);
+    void delete(long id) throws InvalidInputException;
 
-    T update(T t);
+    T update(T t) throws InvalidInputException;
+
+    T findById(long id) throws InvalidInputException;
 
     T findById(long id);
 }
