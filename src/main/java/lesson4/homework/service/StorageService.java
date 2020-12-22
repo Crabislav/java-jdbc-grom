156a4a7e2a4a961a4d9b2f702042c113e8b19d5b
Crabislav <alex.kopnin2@gmail.com>
2020-12-22 22:38:15 +0200

    add implementation of Service.java to StorageService.java

--- src/main/java/lesson4/homework/service/StorageService.java
+++ src/main/java/lesson4/homework/service/StorageService.java
@@ -4,46 +4,39 @@ import lesson4.homework.dao.StorageDAO;
 import lesson4.homework.exception.InvalidInputException;
 import lesson4.homework.model.Storage;
 
-public class StorageService {
+public class StorageService implements Service<Storage> {
     private static final StorageDAO STORAGE_DAO = new StorageDAO();
 
+    @Override
     public Storage save(Storage storage) throws InvalidInputException {
         validateStorage(storage);
         return STORAGE_DAO.save(storage);
     }
 
+    @Override
     public void delete(long id) throws InvalidInputException {
         checkId(id);
         STORAGE_DAO.delete(id);
     }
 
+    @Override
     public Storage update(Storage storage) throws InvalidInputException {
         validateStorage(storage);
         return STORAGE_DAO.update(storage);
     }
 
+    @Override
     public Storage findById(long id) throws InvalidInputException {
         checkId(id);
         return STORAGE_DAO.findById(id);
     }
 
-    private void checkId(long id) throws InvalidInputException {
-        if (id < 0) {
-            throw new InvalidInputException("Id must be equals or greater than 0");
-        }
-    }
-
     private void validateStorage(Storage storage) throws InvalidInputException {
-        if (storage == null) {
-            throw new InvalidInputException("Input can't be null");
-        }
-
-        if (storage.getId() < -1) {
-            throw new InvalidInputException("Id must be 0 or greater");
-        }
+        checkForNull(storage);
+        checkId(storage.getId());
 
         if (storage.getStorageMaxSize() <= 0) {
-            throw new InvalidInputException("Storage's size must be greater than 0");
+            throw new InvalidInputException("Size must be equals 0 or greater");
         }
 
         if (storage.getStorageCountry() == null || storage.getStorageCountry().isEmpty()) {
