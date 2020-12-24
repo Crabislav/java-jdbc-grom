0cf78c9640925c451ee0128a6c631f424e205acd
Crabislav <alex.kopnin2@gmail.com>
2020-12-24 20:50:33 +0200

    add new Exceptions

---
+++ src/main/java/lesson4/homework/exception/IncompatibleFormatsException.java
@@ -0,0 +1,8 @@
+package lesson4.homework.exception;
+
+public class IncompatibleFormatsException extends Exception {
+
+    public IncompatibleFormatsException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/lesson4/homework/exception/NoSpaceException.java
@@ -0,0 +1,7 @@
+package lesson4.homework.exception;
+
+public class NoSpaceException extends Exception{
+    public NoSpaceException(String message) {
+        super(message);
+    }
+}
