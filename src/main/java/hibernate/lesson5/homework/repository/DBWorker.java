7be39c0f8234eb1fc1e5e23204dd105aefb91070
Crabislav <alex.kopnin2@gmail.com>
2021-01-07 15:46:11 +0200

    remove DBWorker public modifier

--- src/main/java/hibernate/lesson5/homework/repository/DBWorker.java
+++ src/main/java/hibernate/lesson5/homework/repository/DBWorker.java
@@ -3,6 +3,6 @@ package hibernate.lesson5.homework.repository;
 import org.hibernate.Session;
 
 @FunctionalInterface
-public interface DBWorker {
+interface DBWorker {
     void execute(Session session);
 }
