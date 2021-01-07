d5b3aee81026af2900f18a12cfb264dfa8a2f12e
Crabislav <alex.kopnin2@gmail.com>
2021-01-07 16:33:02 +0200

    remove comments

--- src/main/java/hibernate/lesson6/productdaohql/Demo.java
+++ src/main/java/hibernate/lesson6/productdaohql/Demo.java
@@ -4,12 +4,12 @@ public class Demo {
     public static void main(String[] args) {
         ProductDAO productDAO = new ProductDAO();
 
-//        System.out.println(productDAO.findById(49));
-//        System.out.println(productDAO.findByName("single"));
-//        System.out.println(productDAO.findByContainedName("tes"));
-//        System.out.println(productDAO.findByPrice(30, 10));
-//        System.out.println(productDAO.findByNameSortedAsc("prod"));
-//        System.out.println(productDAO.findByNameSortedDesc("prod"));
+        System.out.println(productDAO.findById(49));
+        System.out.println(productDAO.findByName("single"));
+        System.out.println(productDAO.findByContainedName("tes"));
+        System.out.println(productDAO.findByPrice(30, 10));
+        System.out.println(productDAO.findByNameSortedAsc("prod"));
+        System.out.println(productDAO.findByNameSortedDesc("prod"));
         System.out.println(productDAO.findByPriceSortedDesc(30, 10));
     }
 }
