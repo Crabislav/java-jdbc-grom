024825c50b1f05a9c09367a06c92ac1171d0d3ef
Crabislav <alex.kopnin2@gmail.com>
2021-01-24 17:28:20 +0200

    add IncompatibleFormatsException

--- src/main/java/jdbc/lesson4/homework/exception/IncompatibleFormatsException.java
+++ src/main/java/jdbc/lesson4/homework/exception/IncompatibleFormatsException.java
@@ -1,7 +1,6 @@
 package jdbc.lesson4.homework.exception;
 
 public class IncompatibleFormatsException extends Exception {
-
     public IncompatibleFormatsException(String message) {
         super(message);
     }
