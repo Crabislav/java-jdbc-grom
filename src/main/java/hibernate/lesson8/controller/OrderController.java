eb008b3fb3457527ede7f94ba7388ea3b9169b61
Crabislav <alex.kopnin2@gmail.com>
2021-01-31 17:59:25 +0200

    add OrderController

---
+++ src/main/java/hibernate/lesson8/controller/OrderController.java
@@ -0,0 +1,36 @@
+package hibernate.lesson8.controller;
+
+import hibernate.lesson8.entities.Order;
+import hibernate.lesson8.service.OrderService;
+
+import java.util.Optional;
+
+public class OrderController implements Controller<Order> {
+    private static final OrderController ORDER_CONTROLLER = getInstance();
+    private static final OrderService ORDER_SERVICE = OrderService.getInstance();
+
+    @Override
+    public Order save(Order order) {
+        return null;
+    }
+
+    @Override
+    public void delete(Order order) {
+
+    }
+
+    @Override
+    public Order update(Order order) {
+        return null;
+    }
+
+    @Override
+    public Order findById(Order order) {
+        return null;
+    }
+
+    public static OrderController getInstance() {
+        return Optional.ofNullable(ORDER_CONTROLLER)
+                .orElseGet(OrderController::new);
+    }
+}
