9d5fbadf34d502ff73e36aa5689985c582a1af93
Crabislav <alex.kopnin2@gmail.com>
2021-02-07 18:40:05 +0200

    [lesson8.dao]: add logic to OrderDAO methods

--- src/main/java/hibernate/lesson8/dao/OrderDAO.java
+++ src/main/java/hibernate/lesson8/dao/OrderDAO.java
@@ -3,28 +3,37 @@ package hibernate.lesson8.dao;
 import hibernate.lesson8.entities.Order;
 
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class OrderDAO implements DAO<Order> {
     private static final OrderDAO ORDER_DAO = getInstance();
 
     @Override
     public Order save(Order order) {
-        return null;
+        DAO.executeInsideTransaction(session -> session.save(order));
+        return order;
     }
 
     @Override
-    public void delete(Order order) {
-
+    public void delete(long id) {
+        Order order = new Order();
+        order.setId(id);
+        DAO.executeInsideTransaction(session -> session.delete(order));
     }
 
     @Override
     public Order update(Order order) {
-        return null;
+        DAO.executeInsideTransaction(session -> session.update(order));
+        return order;
     }
 
     @Override
-    public Order findById() {
-        return null;
+    public Optional<Order> findById(long id) {
+        AtomicReference<Order> order = new AtomicReference<>(new Order());
+        DAO.executeInsideTransaction(session -> order.set(
+                session.find(Order.class, id)
+        ));
+        return Optional.ofNullable(order.get());
     }
 
     public static OrderDAO getInstance() {
