d0d45ce2ebf9943f2ef9d35aeda8885009cb3144
Crabislav <alex.kopnin2@gmail.com>
2021-02-07 23:23:05 +0200

    [lesson8.entities]: change constructor, add hashcode and equals to Hotel

--- src/main/java/hibernate/lesson8/entities/Hotel.java
+++ src/main/java/hibernate/lesson8/entities/Hotel.java
@@ -16,13 +16,12 @@ public class Hotel {
     public Hotel() {
     }
 
-    public Hotel(Long id, String name, String country, String city, String street, List<Room> rooms) {
+    public Hotel(Long id, String name, String country, String city, String street) {
         this.id = id;
         this.name = name;
         this.country = country;
         this.city = city;
         this.street = street;
-        this.rooms = rooms;
     }
 
     @Id
@@ -71,12 +70,38 @@ public class Hotel {
         this.street = street;
     }
 
-    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "hotel")
+    @OneToMany(fetch = FetchType.EAGER, targetEntity = Room.class, cascade = CascadeType.ALL, mappedBy = "hotel")
     public List<Room> getRooms() {
         return rooms;
     }
 
     public void setRooms(List<Room> rooms) {
         this.rooms = rooms;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Hotel hotel = (Hotel) o;
+
+        if (id != null ? !id.equals(hotel.id) : hotel.id != null) return false;
+        if (name != null ? !name.equals(hotel.name) : hotel.name != null) return false;
+        if (country != null ? !country.equals(hotel.country) : hotel.country != null) return false;
+        if (city != null ? !city.equals(hotel.city) : hotel.city != null) return false;
+        if (street != null ? !street.equals(hotel.street) : hotel.street != null) return false;
+        return rooms != null ? rooms.equals(hotel.rooms) : hotel.rooms == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (country != null ? country.hashCode() : 0);
+        result = 31 * result + (city != null ? city.hashCode() : 0);
+        result = 31 * result + (street != null ? street.hashCode() : 0);
+        result = 31 * result + (rooms != null ? rooms.hashCode() : 0);
+        return result;
     }
 }
