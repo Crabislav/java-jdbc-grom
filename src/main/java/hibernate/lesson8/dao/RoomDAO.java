790abdeb10382175d716436db7015cbc8af0ecea
Crabislav <alex.kopnin2@gmail.com>
2021-02-07 18:40:31 +0200

    [lesson8.dao]: add logic to RoomDAO methods

--- src/main/java/hibernate/lesson8/dao/RoomDAO.java
+++ src/main/java/hibernate/lesson8/dao/RoomDAO.java
@@ -3,28 +3,37 @@ package hibernate.lesson8.dao;
 import hibernate.lesson8.entities.Room;
 
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class RoomDAO implements DAO<Room> {
     private static final RoomDAO ROOM_DAO = getInstance();
 
     @Override
     public Room save(Room room) {
-        return null;
+        DAO.executeInsideTransaction(session -> session.save(room));
+        return room;
     }
 
     @Override
-    public void delete(Room room) {
-
+    public void delete(long id) {
+        Room room = new Room();
+        room.setId(id);
+        DAO.executeInsideTransaction(session -> session.delete(room));
     }
 
     @Override
     public Room update(Room room) {
-        return null;
+        DAO.executeInsideTransaction(session -> session.update(room));
+        return room;
     }
 
     @Override
-    public Room findById() {
-        return null;
+    public Optional<Room> findById(long id) {
+        AtomicReference<Room> room = new AtomicReference<>(new Room());
+        DAO.executeInsideTransaction(session -> room.set(
+                session.find(Room.class, id)
+        ));
+        return Optional.ofNullable(room.get());
     }
 
     public static RoomDAO getInstance() {
