289d04e451d75ccd5ccec0399c4726e5008f10d1
Crabislav <alex.kopnin2@gmail.com>
2021-02-07 18:48:29 +0200

    [lesson8.service]: add validataion logic to HotelService methods

--- src/main/java/hibernate/lesson8/service/HotelService.java
+++ src/main/java/hibernate/lesson8/service/HotelService.java
@@ -2,6 +2,9 @@ package hibernate.lesson8.service;
 
 import hibernate.lesson8.dao.HotelDAO;
 import hibernate.lesson8.entities.Hotel;
+import hibernate.lesson8.exceptions.NoAuthorizedUserException;
+import hibernate.lesson8.exceptions.NotEnoughRightsUserException;
+import hibernate.lesson8.usersession.UserSessionManager;
 
 import java.util.Optional;
 
@@ -11,26 +14,76 @@ public class HotelService implements Service<Hotel> {
 
     @Override
     public Hotel save(Hotel hotel) {
-        return null;
+        if (!UserSessionManager.isUserLoggedIn()) {
+            throw new NoAuthorizedUserException("User is not authorized");
+        }
+
+        if (!UserSessionManager.isUserAdmin()) {
+            throw new NotEnoughRightsUserException("Not enough rights to perform this action");
+        }
+
+        Hotel hotelToSave = getFilteredOptional(hotel)
+                .orElseThrow(() -> new IllegalArgumentException("Input object has an invalid field"));
+        return HOTEL_DAO.save(hotelToSave);
     }
 
     @Override
-    public void delete(Hotel hotel) {
+    public void delete(long id) {
+        if (!UserSessionManager.isUserLoggedIn()) {
+            throw new NoAuthorizedUserException("User is not authorized");
+        }
 
+        if (!UserSessionManager.isUserAdmin()) {
+            throw new NotEnoughRightsUserException("Not enough rights to perform this action");
+        }
+
+        if (id < 1) {
+            throw new IllegalArgumentException("Id(" + id + ") can't be < 1");
+        }
+        HOTEL_DAO.delete(id);
     }
 
     @Override
     public Hotel update(Hotel hotel) {
-        return null;
+        if (!UserSessionManager.isUserLoggedIn()) {
+            throw new NoAuthorizedUserException("User is not authorized");
+        }
+
+        if (!UserSessionManager.isUserAdmin()) {
+            throw new NotEnoughRightsUserException("Not enough rights to perform this action");
+        }
+
+        Hotel hotelToUpdate = getFilteredOptional(hotel)
+                .filter(input -> input.getRooms() != null)
+                .orElseThrow(() -> new IllegalArgumentException("Input object has an invalid field"));
+
+        return HOTEL_DAO.update(hotelToUpdate);
     }
 
     @Override
-    public Hotel findById(Hotel hotel) {
-        return null;
+    public Optional<Hotel> findById(long id) {
+        if (!UserSessionManager.isUserLoggedIn()) {
+            throw new NoAuthorizedUserException("User is not authorized");
+        }
+
+        if (id < 1) {
+            throw new IllegalArgumentException("Input id (id" + id + ") can't be lower than 1");
+        }
+        return HOTEL_DAO.findById(id);
     }
 
     public static HotelService getInstance() {
-        return Optional.ofNullable(HOTEL_SERVICE)
-                .orElseGet(HotelService::new);
+        return Optional.ofNullable(HOTEL_SERVICE).orElseGet(HotelService::new);
     }
+
+    private Optional<Hotel> getFilteredOptional(Hotel hotel) {
+        return Optional.ofNullable(hotel)
+                .filter(input -> input.getId() != null && input.getId() > 0)
+                .filter(input -> input.getName() != null && !input.getName().isEmpty())
+                .filter(input -> input.getCountry() != null && !input.getCountry().isEmpty())
+                .filter(input -> input.getCity() != null && !input.getCity().isEmpty())
+                .filter(input -> input.getStreet() != null && !input.getStreet().isEmpty());
+    }
+
+
 }
