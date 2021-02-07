37c1f0dca35740de8a3920b0e239e6bcc0936723
Crabislav <alex.kopnin2@gmail.com>
2021-02-07 23:30:15 +0200

    [lesson8.service.UserService]: add logic to methods

--- src/main/java/hibernate/lesson8/service/UserService.java
+++ src/main/java/hibernate/lesson8/service/UserService.java
@@ -2,6 +2,8 @@ package hibernate.lesson8.service;
 
 import hibernate.lesson8.dao.UserDAO;
 import hibernate.lesson8.entities.User;
+import hibernate.lesson8.exceptions.NoAuthorizedUserException;
+import hibernate.lesson8.usersession.UserSessionManager;
 
 import java.util.Optional;
 
@@ -11,26 +13,58 @@ public class UserService implements Service<User> {
 
     @Override
     public User save(User user) {
-        return null;
+        User userToSave = getFilteredOptional(user)
+                .orElseThrow(() -> new IllegalArgumentException("Input object has an invalid field"));
+        return USER_DAO.save(userToSave);
     }
 
     @Override
-    public void delete(User user) {
+    public void delete(long id) {
+        if (!UserSessionManager.isUserLoggedIn()) {
+            throw new NoAuthorizedUserException("User is not authorized");
+        }
 
+        if (id < 1) {
+            throw new IllegalArgumentException("Id(" + id + ") can't be < 1");
+        }
+
+        USER_DAO.delete(id);
     }
 
     @Override
     public User update(User user) {
-        return null;
+        if (!UserSessionManager.isUserLoggedIn()) {
+            throw new NoAuthorizedUserException("User is not authorized");
+        }
+
+        User userToUpdate = getFilteredOptional(user)
+                .orElseThrow(() -> new IllegalArgumentException("Input object has an invalid field"));
+
+        return USER_DAO.update(userToUpdate);
     }
 
     @Override
-    public User findById(User user) {
-        return null;
+    public Optional<User> findById(long id) {
+        if (!UserSessionManager.isUserLoggedIn()) {
+            throw new NoAuthorizedUserException("User is not authorized");
+        }
+
+        if (id < 1) {
+            throw new IllegalArgumentException("Input id (id" + id + ") can't be lower than 1");
+        }
+
+        return USER_DAO.findById(id);
     }
 
     public static UserService getInstance() {
-        return Optional.ofNullable(USER_SERVICE)
-                .orElseGet(UserService::new);
+        return Optional.ofNullable(USER_SERVICE).orElseGet(UserService::new);
+    }
+
+    private Optional<User> getFilteredOptional(User user) {
+        return Optional.ofNullable(user)
+                .filter(input -> input.getId() != null && input.getId() > 0)
+                .filter(input -> input.getUserName() != null && !input.getUserName().isEmpty())
+                .filter(input -> input.getPassword() != null && !input.getPassword().isEmpty())
+                .filter(input -> input.getCountry() != null && !input.getCountry().isEmpty());
     }
 }
