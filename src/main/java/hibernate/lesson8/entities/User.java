f160ebbd9ca0b7af4bc05eb0c17bdfd5a105d4a9
Crabislav <alex.kopnin2@gmail.com>
2021-02-07 23:25:27 +0200

    [lesson8.entities.User]: change constructor; add hashcode and equals;

--- src/main/java/hibernate/lesson8/entities/User.java
+++ src/main/java/hibernate/lesson8/entities/User.java
@@ -18,13 +18,12 @@ public class User {
     public User() {
     }
 
-    public User(Long id, String userName, String password, String country, UserType userType, List<Order> orders) {
+    public User(Long id, String userName, String password, String country, UserType userType) {
         this.id = id;
         this.userName = userName;
         this.password = password;
         this.country = country;
         this.userType = userType;
-        this.orders = orders;
     }
 
     @Id
@@ -74,13 +73,39 @@ public class User {
         this.userType = userType;
     }
 
-    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "userOrdered")
+    @OneToMany(fetch = FetchType.EAGER, targetEntity = Order.class, cascade = CascadeType.ALL, mappedBy = "userOrdered")
     public List<Order> getOrders() {
         return orders;
     }
 
     public void setOrders(List<Order> orders) {
         this.orders = orders;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != null ? !id.equals(user.id) : user.id != null) return false;
+        if (userName != null ? !userName.equals(user.userName) : user.userName != null) return false;
+        if (password != null ? !password.equals(user.password) : user.password != null) return false;
+        if (country != null ? !country.equals(user.country) : user.country != null) return false;
+        if (userType != user.userType) return false;
+        return orders != null ? orders.equals(user.orders) : user.orders == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (userName != null ? userName.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (country != null ? country.hashCode() : 0);
+        result = 31 * result + (userType != null ? userType.hashCode() : 0);
+        result = 31 * result + (orders != null ? orders.hashCode() : 0);
+        return result;
     }
 }
 
