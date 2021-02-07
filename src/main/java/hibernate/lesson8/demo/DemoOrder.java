eb55a4701497e83cdcfaf16eb5f2a930c5e3077b
Crabislav <alex.kopnin2@gmail.com>
2021-02-07 23:20:25 +0200

    [lesson8.demo]: DemoOrder fix

--- src/main/java/hibernate/lesson8/demo/DemoOrder.java
+++ src/main/java/hibernate/lesson8/demo/DemoOrder.java
@@ -3,6 +3,7 @@ package hibernate.lesson8.demo;
 import hibernate.lesson8.dao.HotelDAO;
 import hibernate.lesson8.dao.OrderDAO;
 import hibernate.lesson8.dao.RoomDAO;
+import hibernate.lesson8.dao.UserDAO;
 import hibernate.lesson8.entities.*;
 
 import java.util.Date;
@@ -11,20 +12,32 @@ public class DemoOrder {
     private static final OrderDAO ORDER_DAO = OrderDAO.getInstance();
     private static final HotelDAO HOTEL_DAO = HotelDAO.getInstance();
     private static final RoomDAO ROOM_DAO = RoomDAO.getInstance();
+    private static final UserDAO USER_DAO = UserDAO.getInstance();
 
-    private static Hotel testHotel = new Hotel(1L, "Hotel", "UA", "Kiev", "Street", null);
-    private static Room testRoom = new Room(1L, 2, 50, true, false, new Date(), testHotel);
-    private static User testAdminUser = new User(1L, "Admin", "1234", "UA", UserType.ADMIN, null);
-    private static Order testOrder = new Order(1, testAdminUser, testRoom, new Date(1_220_227_200L * 1000), new Date(1_226_000_000L * 1000), 6000d);
+    private static Hotel testHotel = new Hotel(1L, "Hotel", "UA", "Kiev", "Street");
+    private static Room testRoom = new Room(1L, 2, 50, true, false, new Date(), null);
+    private static User testAdminUser = new User(1L, "Admin", "1234", "UA", UserType.ADMIN);
+    private static Order testOrder = new Order(1, null, null, new Date(1_220_227_200L * 1000), new Date(1_226_000_000L * 1000), 6000d);
 
     public static void main(String[] args) {
+        //hotel init
+        HOTEL_DAO.save(testHotel);
+        testRoom.setHotel(HOTEL_DAO.findById(testHotel.getId()).get());
+        //room init
+        ROOM_DAO.save(testRoom);
+        testOrder.setRoom(ROOM_DAO.findById(testRoom.getId()).get());
+        //user init
+        USER_DAO.save(testAdminUser);
+        testOrder.setUserOrdered(USER_DAO.findById(testAdminUser.getId()).get());
+
         testRoomDAO();
+
+        USER_DAO.delete(testAdminUser.getId());
+        ROOM_DAO.delete(testRoom.getId());
+        HOTEL_DAO.delete(testHotel.getId());
     }
 
     private static void testRoomDAO() {
-        HOTEL_DAO.save(testHotel);
-        ROOM_DAO.save(testRoom);
-
         boolean result;
 
         //save
@@ -45,10 +58,6 @@ public class DemoOrder {
         ORDER_DAO.delete(testOrder.getId());
         result = ORDER_DAO.findById(testOrder.getId()).isEmpty();
         System.out.println("delete: " + result);
-
-
-        ROOM_DAO.delete(testRoom.getId());
-        HOTEL_DAO.delete(testHotel.getId());
 
         System.out.println("================================");
     }
