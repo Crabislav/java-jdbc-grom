7dc6cb6150672f5d21ef634de05b3def462282d9
Crabislav <alex.kopnin2@gmail.com>
2021-02-07 18:38:07 +0200

    [lesson8.dao]: add logic to HotelDAO methods

--- src/main/java/hibernate/lesson8/dao/HotelDAO.java
+++ src/main/java/hibernate/lesson8/dao/HotelDAO.java
@@ -3,28 +3,37 @@ package hibernate.lesson8.dao;
 import hibernate.lesson8.entities.Hotel;
 
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class HotelDAO implements DAO<Hotel> {
     private static final HotelDAO HOTEL_DAO = getInstance();
 
     @Override
     public Hotel save(Hotel hotel) {
-        return null;
+        DAO.executeInsideTransaction(session -> session.save(hotel));
+        return hotel;
     }
 
     @Override
-    public void delete(Hotel hotel) {
-
+    public void delete(long id) {
+        Hotel hotel = new Hotel();
+        hotel.setId(id);
+        DAO.executeInsideTransaction(session -> session.delete(hotel));
     }
 
     @Override
     public Hotel update(Hotel hotel) {
-        return null;
+        DAO.executeInsideTransaction(session -> session.update(hotel));
+        return hotel;
     }
 
     @Override
-    public Hotel findById() {
-        return null;
+    public Optional<Hotel> findById(long id) {
+        AtomicReference<Hotel> hotel = new AtomicReference<>(new Hotel());
+        DAO.executeInsideTransaction(session -> hotel.set(
+                session.find(Hotel.class, id)
+        ));
+        return Optional.of(hotel.get());
     }
 
     public static HotelDAO getInstance() {
