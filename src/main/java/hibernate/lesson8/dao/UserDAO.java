4d4932e704328f1c23752fb14ca00b23dad100fa
Crabislav <alex.kopnin2@gmail.com>
2021-02-07 18:41:04 +0200

    [lesson8.dao]: add logic to UserDAO methods

--- src/main/java/hibernate/lesson8/dao/UserDAO.java
+++ src/main/java/hibernate/lesson8/dao/UserDAO.java
@@ -3,31 +3,39 @@ package hibernate.lesson8.dao;
 import hibernate.lesson8.entities.User;
 
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
 
-public class UserDAO implements DAO<User>{
+public class UserDAO implements DAO<User> {
+
     private static final UserDAO USER_DAO = getInstance();
 
     @Override
     public User save(User user) {
-        return null;
+        DAO.executeInsideTransaction(session -> session.save(user));
+        return user;
     }
 
     @Override
-    public void delete(User user) {
-
+    public void delete(long id) {
+        DAO.executeInsideTransaction(session -> session.delete(id));
     }
 
     @Override
     public User update(User user) {
-        return null;
+        DAO.executeInsideTransaction(session -> session.update(user));
+        return user;
     }
 
     @Override
-    public User findById() {
-        return null;
+    public Optional<User> findById(long id) {
+        AtomicReference<User> user = new AtomicReference<>(new User());
+        DAO.executeInsideTransaction(session -> user.set(
+                session.find(User.class, id)
+        ));
+        return Optional.ofNullable(user.get());
     }
 
-    public static UserDAO getInstance(){
+    public static UserDAO getInstance() {
         return Optional.ofNullable(USER_DAO)
                 .orElseGet(UserDAO::new);
     }
