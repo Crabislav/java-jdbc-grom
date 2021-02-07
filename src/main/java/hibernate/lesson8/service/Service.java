3512d833b736de62a825b3cd59981e220650e264
Crabislav <alex.kopnin2@gmail.com>
2021-02-07 18:47:49 +0200

    [lesson8.service]: change methods signatures at Service

--- src/main/java/hibernate/lesson8/service/Service.java
+++ src/main/java/hibernate/lesson8/service/Service.java
@@ -1,13 +1,13 @@
 package hibernate.lesson8.service;
 
+import java.util.Optional;
+
 public interface Service<T> {
     T save(T t);
 
-    void delete(T t);
+    void delete(long id);
 
     T update(T t);
 
-    T findById(T t);
-
-
+    Optional<T> findById(long id);
 }
